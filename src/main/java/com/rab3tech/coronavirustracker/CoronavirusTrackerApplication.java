52b444992b3abb86b31d8d6568a9ae0b2494157f
Yubi48 <yubi48@gmail.com>
2020-10-08 21:18:32 -0400

    first commit

---
+++ src/main/java/com/rab3tech/coronavirustracker/CoronavirusTrackerApplication.java
@@ -0,0 +1,15 @@
+package com.rab3tech.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;	
+
+@SpringBootApplication
+public class CoronavirusTrackerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CoronavirusTrackerApplication.class, args);
+		
+	}
+
+}
---
+++ src/main/java/com/rab3tech/coronavirustracker/controller/HomeController.java
@@ -0,0 +1,31 @@
+package com.rab3tech.coronavirustracker.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.rab3tech.coronavirustracker.model.LocationStats;
+import com.rab3tech.coronavirustracker.service.CoronaVirusDataService;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private CoronaVirusDataService coronaVirusDataService;
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+		int totalCases=allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+		int totalNewCases=allStats.stream().mapToInt(stat->stat.getDiffFromPrevDay()).sum();
+		model.addAttribute("locationStats",allStats);
+		model.addAttribute("totalCases",totalCases);
+		model.addAttribute("totalNewCase",totalNewCases);
+		
+		return "home";
+	}
+	
+}
